6019e92d90d52c04d44230e173f8de9641ad9045
Derrick Ding <yingke.dev@gmail.com>
2022-11-07 17:19:19 -0800

    add docstrings for tests

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ import logic
 class TestLogic(unittest.TestCase):
 
     def test_get_winner(self):
+        """
+        Given a given board for X to win, check if the get_winner function works.
+        """
         board = [
             ['X', None, 'O'],
             [None, 'X', None],
@@ -13,6 +16,9 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(logic.get_winner(board), 'X')
 
     def test_make_empty_board(self):
+        """
+        Given a correct empty board, check if the make_empty_board() function works.
+        """
         correct_board = [
             [None, None, None],
             [None, None, None],
@@ -22,6 +28,9 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(logic.make_empty_board(), correct_board)
 
     def test_other_player(self):
+        """
+        Given a current player X, check if the other_player() function replies with a O. And check otherwise.
+        """
         self.assertEqual(logic.other_player("X"), "O")
         self.assertEqual(logic.other_player("O"), "X")
 
