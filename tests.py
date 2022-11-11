62ea06323e1d2ae52945aa441bb49f2454ea596f
Derrick Ding <yingke.dev@gmail.com>
2022-11-11 12:05:15 -0800

    add tests for game

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
 import logic
+import utils
+from game import SingleModeGame
 
 
 class TestLogic(unittest.TestCase):
@@ -33,6 +35,49 @@ class TestLogic(unittest.TestCase):
         """
         self.assertEqual(logic.other_player("X"), "O")
         self.assertEqual(logic.other_player("O"), "X")
+
+
+class TestUtils(unittest.TestCase):
+    def test_validate_game_mode_input(self):
+        """
+        Test game mode input validation function works properly.
+        :return:
+        """
+        self.assertTrue(utils.validate_game_mode_input("1"))
+        self.assertTrue(utils.validate_game_mode_input("2"))
+
+        self.assertFalse(utils.validate_game_mode_input("abc"))
+        self.assertFalse(utils.validate_game_mode_input("-30"))
+
+    def test_validate_input(self):
+        """
+        Test player move coordinate input validation function works properly.
+        :return:
+        """
+        test_board = [
+            ['X', None, 'O'],
+            [None, None, None],
+            [None, 'O', 'X'],
+        ]
+
+        self.assertTrue(utils.validate_input(test_board, "1", "1"))
+        self.assertFalse(utils.validate_input(test_board, "3", "3"))
+        self.assertFalse(utils.validate_input(test_board, "0", "0"))
+
+
+class TestGame(unittest.TestCase):
+    def test_bot_random_step(self):
+        """
+        Test bot random step function works properly.
+        :return:
+        """
+        game = SingleModeGame()
+        game.set_board(0, 0, "X")
+        game.set_board(0, 1, "O")
+        game.set_board(0, 2, "X")
+
+        # Bot's next step should be in a list with (0, 0), (0, 1) and (0, 2) excluded.
+        self.assertIn(game.bot_random_step(), [(1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
 
 
 if __name__ == '__main__':
